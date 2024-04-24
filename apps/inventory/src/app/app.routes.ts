db6335426176e0925a6b4c78de5599408f7b27c7
evgenjnr <evgeniyr7@gmail.com>
2024-04-24 20:12:13 +0300

    setup baseurl and diff port for inventory serve script

--- apps/inventory/src/app/app.routes.ts
+++ apps/inventory/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
+  {
+    path: 'prod',
+    loadComponent: () =>
+      import('@nx-tutorial-estore/products').then(
+        (c) => c.ProductListComponent
+      ),
+  },
+];
